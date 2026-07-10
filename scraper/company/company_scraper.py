96b61a91647b94e4b2dc506e3d50e4c0046f8712
vijay-nagar <vijay.nagar@cacheflow.in>
2026-07-10 12:10:44 +0530

    Updated company scraper and product schema

--- scraper/company/company_scraper.py
+++ scraper/company/company_scraper.py
@@ -1,17 +1,43 @@
 import json
 
-
 def load_company_data():
+
     with open(
         "data/company/apple_company.json",
         "r"
     ) as file:
-        data = json.load(file)
 
-    return data
+        company_data = json.load(file)
+
+    return company_data
+
+def display_company_info(company):
+
+    print("\nApple Company Information")
+    print("-" * 50)
+
+    print(f"Company Name : {company['company_name']}")
+    print(f"Founded      : {company['founded']}")
+    print(
+        f"Headquarters : "
+        f"{company['headquarters']['city']}, "
+        f"{company['headquarters']['state']}, "
+        f"{company['headquarters']['country']}"
+    )
+
+    print("\nFounders:")
+
+    for founder in company["founders"]:
+        print(f"• {founder}")
+
+    print("\nBusiness Segments:")
+
+    for segment in company["major_business_segments"]:
+        print(f"• {segment}")
 
 
 if __name__ == "__main__":
+
     company = load_company_data()
 
-    print(company)
+    display_company_info(company)
